c4066a4a10aeafe3637dd45d28cb0c8b405ea707
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-06-28 15:31:21 +0800

    feat: Create a responsive grid column

---
+++ component/AnimeLists.tsx
@@ -0,0 +1,19 @@
+import Card from "./Card";
+import { Grid } from "@chakra-ui/react";
+
+const AnimeLists = () => {
+  
+
+  return (
+    <Grid templateColumns={{
+      base: 'repeat(1, 1fr)',
+      sm: 'repeat(2, 1fr)',
+      md: 'repeat(3, 1fr)',
+      lg: 'repeat(5, 1fr)'
+    }} gap={6} p="4" w="100%">
+      <Card />
+    </Grid>
+  );
+};
+
+export default AnimeLists;
