2f4ccb461b0f1cb32609cb9fa5c3b3f17680838f
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-06-28 13:12:22 +0800

    feat: Create initial view page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: `https://graphql.anilist.co`,
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider client={client}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
