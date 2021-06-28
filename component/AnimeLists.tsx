402fba07d6721053cc3b6402d3d9510f85756e8d
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-06-28 15:58:22 +0800

    feat: responsive card

--- component/AnimeLists.tsx
+++ component/AnimeLists.tsx
@@ -9,7 +9,8 @@ const AnimeLists = () => {
       base: 'repeat(1, 1fr)',
       sm: 'repeat(2, 1fr)',
       md: 'repeat(3, 1fr)',
-      lg: 'repeat(5, 1fr)'
+      lg: 'repeat(4, 1fr)',
+      xl: 'repeat(5, 1fr)'
     }} gap={6} p="4" w="100%">
       <Card />
     </Grid>
